c7eed10ee218e907c72bd1f9639797049335be42
mojtaba <mojtabamoosaei6@gmail.com>
2024-03-25 16:14:52 +0330

    compact in route

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome') ->with('name' , 'mojtaba');
+    return view('welcome') ->with(['name'=>'mojtaba' , 'famil'=> 'moosaei']);
 });
 
+Route::get('/home', function () {
+    $var = "test"; 
+    return view('welcome' , compact('var'));
+});
 
 // Route::group(['prefix' => 'admin/panel' ], function(){
 //     Route::get('/' , function(){
-//         return "dashbord";});]
+//         return "dashbord";});
 //     Route::get('menu' , function(){
 //         return "admin/menu";});
 // });
