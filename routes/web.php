a0dfd29bdc1aa25647d1d092f75ee9dc80b6a703
mojtaba <mojtabamoosaei6@gmail.com>
2024-03-25 16:08:01 +0330

    prefix and domain

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome') ->with('name' , 'mojtaba');
 });
+
+
+// Route::group(['prefix' => 'admin/panel' ], function(){
+//     Route::get('/' , function(){
+//         return "dashbord";});]
+//     Route::get('menu' , function(){
+//         return "admin/menu";});
+// });
+
+
+// Route::domain('test.localhost.com')->group(function()
+// {
+//     Route::get('/' , function(){
+//         return "admin/menu";});
+// });
+
+
 
 
