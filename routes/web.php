5f842fee5384e3f132b7eb1b847ebe236e9b9ac4
mojtaba <mojtabamoosaei6@gmail.com>
2024-03-26 05:05:35 +0330

    laravel11-redirect-form

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
+Route::resource('home' , HomeController::class); //*   باعث میشه تمامی متدهای این کنترلز رو برای کا ایجاد کنه
+// Route::get('/home', [HomeController::class ,'index']);  //* 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-// Route::get('/' , "CategoryController@index");
+Route::get('/home', [HomeController::class ,'store']);  //* 
 
-// Route::resource('/category' , 'CategoryController');
+
+
+
+// Route::redirect('adres-vorodi' , 'home/create');
+
+Route::get('login', function () {     // اگرب بخام به صفحه ای برم که باید پارامتری رو خم برا خودم ببرم ازین استفاده میشه 
+    // return redirect('home/create');
+    return view('form');
+});
+
+
+// Route::get('adres-vorodi', function () {     // اگرب بخام به صفحه ای برم که باید پارامتری رو خم برا خودم ببرم ازین استفاده میشه 
+//     return redirect()->route('home.create', ['name'=>'mojtaba']);
+//  // **
+// });
+
+
+
+
+
+
+
 
 // Route::get('/', function () {
 //     return view('welcome') ->with(['name'=>'mojtaba' , 'famil'=> 'moosaei']);
